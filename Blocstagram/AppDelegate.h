360901965a77686ff6e0974c27b4b38873262a69
Jeffrey Isaray <ji8521@gmail.com>
2015-07-28 19:21:26 -0400

    Initial Blocstagram commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Jeffrey Isaray on 7/28/15.
+//  Copyright (c) 2015 Jeffrey Isaray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Jeffrey Isaray on 7/28/15.
+//  Copyright (c) 2015 Jeffrey Isaray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
